d523129016d5332f30aba413fd19c444f3ba8adb
petriQore <jeffbanarie123@gmail.com>
2024-12-22 17:18:32 +0100

    plus au moins works, fadit, gg

--- back/index.js
+++ back/index.js
@@ -1,27 +1,54 @@
 import { Chess } from 'chess.js';
 import { spawn } from 'child_process';
+import express from 'express';
+const app = express();
+import cors from 'cors';
+app.use(cors());
+const stockfish = spawn("../stockfish.exe"); 
 
-const string = "g4 f6 d4 a6 e4 Kf7 Bc4+ e6 d5 c5 a4 Ra7 Bf4 Qa5+ Nc3 d6 dxe6+ Ke8 Bd2 Bd7 exd7+ Nxd7 f4 Qb6 Nf3 h6 O-O Qd8 Qe2 Qc8 Nd5 h5 gxh5 Ne5 fxe5 dxe5 Qg2 Rh6 Kh1 Qd8 Bxh6 Nxh6 Qg6+ Kd7 Be2 Qa5 Nxe5+ Kd8 Rxf6 gxf6";
+stockfish.stdin.write("uci\n");
+stockfish.stdin.write('setoption name multipv value 5\n');
 
-let moves = string.trim().split(" ");
-const chess = new Chess();
+const port = 3000;
 
-moves.forEach(move => {
-    chess.move(move);
+app.get('/send-data', (req, res) => {
+    const moves = req.query.moves.trim().split(" ");
+    analyzePosition(moves).then(result => {
+        res.json(result);
+    });
 });
 
-const fen = chess.fen();
-// console.log('FEN:', fen);
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
 
-const stockfish = spawn("../stockfish.exe"); 
+
+// const string = "g4 f6 d4 a6 e4 Kf7 Bc4+ e6 d5 c5 a4 Ra7 Bf4 Qa5+ Nc3 d6 dxe6+ Ke8 Bd2 Bd7 exd7+ Nxd7 f4 Qb6 Nf3 h6 O-O Qd8 Qe2 Qc8 Nd5 h5 gxh5 Ne5 fxe5 dxe5 Qg2 Rh6 Kh1 Qd8 Bxh6 Nxh6 Qg6+ Kd7 Be2 Qa5 Nxe5+ Kd8 Rxf6 gxf6";
+
+// let moves = string.trim().split(" ");
+// const chess = new Chess();
+
+// moves.forEach(move => {
+//     chess.move(move);
+// });
+
+// const fen = chess.fen();
+// console.log('FEN:', fen);
 
 const zip = (a, b) => a.map((k, i) => [k, b[i]]);
 
-async function analyzePosition(fen) {
+async function analyzePosition(moves) {
     if (!stockfish) {
       throw new Error('errrrrrrr');
     }
   
+    const chess = new Chess();
+    moves.forEach(move => {
+      chess.move(move);
+  });
+  const fen = chess.fen();
+
+
     return new Promise((resolve) => {
       const score = [];
       const nextmove = [];
@@ -44,20 +71,23 @@ async function analyzePosition(fen) {
         }
         
         const result = zip(nextmove, score);    
+        const uniqueResult = Array.from(
+          new Set(result.map(item => JSON.stringify(item)))
+        ).map(item => JSON.parse(item));
+
 
         if (output.includes('bestmove')) {
-            console.log('Result:', result);
-            resolve(result);
+            console.log('Result:', uniqueResult);
+            resolve(uniqueResult);
         }
       });
   
-      stockfish.stdin.write("uci\n");
-      stockfish.stdin.write('setoption name multipv value 5\n');
+
       stockfish.stdin.write(`position fen ${fen}\n`);
       stockfish.stdin.write("go depth 15\n");
     });
   }
 
-analyzePosition(fen);  
+// analyzePosition(fen);  
 
-//match score?: score\s(\S*\s*\S*)
+//match score???: score\s(\S*\s*\S*)
